5efdc76989e6c704fb2db8caf68537080b777e26
Liu_Jen_Hao.tw <twjenhao@gmail.com>
2022-10-06 20:03:44 +0800

    Update week4Hw-me.R

--- week3/week4Hw-me.R
+++ week3/week4Hw-me.R
@@ -3,8 +3,72 @@ googlesheets4::read_sheet(
   sheet="data 4"
 ) -> data4
 data4
-library(ggplot2)
-ggplot(data = data4,
-       mapping=(aes(x="country",y="shareProfitable"))
-       )
+
+gg41 <- ggplot(data=data4,
+       )+
+  geom_col(
+    color="White",
+    width=1,
+    mapping = aes(x=country,y=shareProfitable,
+                  fill=isOPEC,
+                  )
+    )+
+  geom_col(
+    alpha=0.3,
+    linetype="f1",
+    color="Gray",
+    width=1,
+    mapping = aes(x=country,y=-shareNonProfitable,
+                  fill=isOPEC,
+                  )
+    )+
+  geom_text(
+    nudge_y = 0.03,
+    aes(x=country,y=shareProfitable,label=country)
+  )
+gg41
+
+
+
+
+
+
+
+gg41Width <- ggplot(
+  data=data4,mapping=
+    aes(x=country,width=3*Volumn/sum(Volumn))
+)+
+  geom_col(
+    color="White",
+    mapping = aes(y=shareProfitable,
+                  fill=isOPEC,
+    )
+  )+
+  geom_col(
+    alpha=0.3,
+    linetype="f1",
+    color="Gray",
+    mapping = aes(y=-shareNonProfitable,
+                  fill=isOPEC,
+    )
+  )+
+  geom_text(
+    nudge_y = 0.03,
+    aes(x=country,y=shareProfitable,label=country)
+  )
+
+gg41Width
+
+
+gg41Width+
+  geom_segment(
+  data=data4,
+  mapping=aes(
+    x=0, y=0,
+    xend=7.5, yend=0
+  ),
+  alpha=0.7,
+  color='black'
+)
+
 
