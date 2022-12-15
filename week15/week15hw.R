069091219eb2afa6c54f8332b7050c4c79f3235a
Liu_Jen_Hao.tw <twjenhao@gmail.com>
2022-12-16 07:21:22 +0800

    Create week15hw.R

---
+++ week15/week15hw.R
@@ -0,0 +1,28 @@
+library(ggplot2)
+library(ggpol)
+library(curl)
+
+
+utils::unzip(url, list = TRUE)
+url <- "https://github.com/twjenhao/111-1-econDV/blob/main/week15/111%E5%B9%B4%E7%9B%B4%E8%BD%84%E5%B8%82%E8%AD%B0%E5%93%A1%E9%81%B8%E8%88%89.xlsx?raw=true"
+# 在url最後要加這一段“?raw=true”執行下面程式才可以下載到檔案
+destfile15 <- "111年直轄市議員選舉.xlsx"
+curl_download(url, destfile15)
+data15 <- read_xlsx(destfile15)
+
+
+bt <- data.frame(
+  parties = factor(c("CDU", "CSU", "AfD", "FDP", "SPD", 
+                     "Linke", "Gruene", "Fraktionslos"),
+                   levels = c("CDU", "CSU", "AfD", "FDP", "SPD", 
+                              "Linke", "Gruene", "Fraktionslos")),
+  seats   = c(200, 46, 92, 80, 153, 69, 67, 2),
+  colors  = c("black", "blue", "lightblue", "yellow", 
+              "red","purple", "green", "grey"),
+  stringsAsFactors = FALSE)
+
+ggplot(data15) + 
+  geom_parliament(aes(seats = seats, fill = parties), color = "black") + 
+  scale_fill_manual(values = bt$colors, labels = bt$parties) +
+  coord_fixed() + 
+  theme_void()
